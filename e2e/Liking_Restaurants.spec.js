3fa24066b94de2a935880d0e57ca6c436e236227
Agnes Glory Siahaan <125450406+agnessia23@users.noreply.github.com>
2023-06-12 06:53:23 +0700

    Update Liking_Restaurants.spec.js

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,10 +1,29 @@
+const assert = require('assert');
+
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
-    I.amOnPage('/#/like');
+    I.amOnPage('/#/favorites');
   });
 
 Scenario('showing empty liked restaurants', ({ I }) => {
-    //I.seeElement('#query');
+   //I.seeElement('#query');
     I.see('There is no Favorit restaurant yet');
+});
+
+Scenario('liking one restaurant', async ({ I }) => {
+  const firstResto = locate('.restaurant-item-name').first();
+  const firstRestoTitle = await I.grabTextFrom(firstResto);
+  I.click(firstResto);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorites');
+  I.waitForElement('.restaurant-item-name');
+  I.seeElement('.restaurant-item');
+  const likedRestoTitle = await I.grabTextFrom('.restaurant-item-name');
+
+  assert.strictEqual(firstRestoTitle, likedRestoTitle);
+  // … kita akan mengisi uji coba berikutnya …
 });
